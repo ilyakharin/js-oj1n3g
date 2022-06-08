c97b49fe286b95d6fea932c8b6990e94dddc07ed
ilyakharin <ilya2021511@gmail.com>
2022-06-08 17:47:47 +0300

    2 files modified, 1 deleted

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const { setBlock } = require('progkids/world');
+const { setBlock, setCuboid } = require('progkids/world');
+const { removeAll } = require('progkids/drones');
 const { connect, clear } = require('progkids/server');
+
 
 const NICK = 'Sup';
 const TOKEN = '5esyLrgTE63CPwi4iE5B';
@@ -11,6 +13,11 @@ const main = async () => {
   });
 
   clear();
+
+  await setCuboid(-63,0,-63,63,25,63,0);
+  await removeAll();
+  await setCuboid(-63,1,-63,63,9,63,10);
+  await setCuboid(0,12,0,20,12,5,1);
 };
 
 document.getElementById('test').addEventListener('click', () => {

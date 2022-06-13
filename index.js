59e8a2fce2efb1d2487900f9009630eaba514f4e
ilyakharin <ilya2021511@gmail.com>
2022-06-13 17:15:03 +0300

    1 file modified

--- index.js
+++ index.js
@@ -140,8 +140,6 @@ const main = async () => {
     token: TOKEN,
   });
 
-  await new Promise((ok) => setTimeout(ok, 1000));
-
   begin();
 
   setCuboid(9, 12, 6, 11, 12, 8, 57);
@@ -151,8 +149,8 @@ const main = async () => {
   setInterval(async () => {
     let pos = getPos();
     let underPos = [pos[0], pos[1] - 1, pos[2]];
-    console.log(await getBlock(underPos[0], underPos[1], underPos[2]));
-    if (await getBlock(underPos[0], underPos[1], underPos[2]) === 57) {
+    console.log(getBlock(underPos[0], underPos[1], underPos[2]));
+    if (getBlock(underPos[0], underPos[1], underPos[2]) === 57) {
       await platform1(9,12,9,1);
     } else if (getBlock(pos[0], pos[1] - 1, pos[2]) == 10) {
       setPos(10,13,1);
